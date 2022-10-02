35f2ce45b719957850fcc7e13cfd9f170a0a0de9
Ben Smith <binjimin@gmail.com>
2022-10-01 18:21:42 -0700

    Fix .tmx files

--- ref/bg.tsx
+++ ref/bg.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="bg" tilewidth="8" tileheight="8" tilecount="128" columns="16">
- <image source="../bg.png" width="128" height="64"/>
+<tileset version="1.9" tiledversion="1.9.1" name="bg" tilewidth="8" tileheight="8" tilecount="128" columns="16">
+ <image source="../img/bg.png" width="128" height="64"/>
 </tileset>
